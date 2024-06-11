918d00099a6a6eb955a0f894573864ef6525843e
Jack Fan <jackfan108@gmail.com>
2024-06-11 05:23:23 -0400

    mkdir for sublime

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ else
   echo -e "✅\033[0;96m Installing Homebrew\033[0m"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jackfan/.zprofile
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
@@ -18,7 +18,7 @@ else
 fi
 
 # install gotham theme on iterm
-if [ -e "/Users/jackfan/Library/Preferences/com.googlecode.iterm2.plist" ] && cat /Users/jackfan/Library/Preferences/com.googlecode.iterm2.plist | grep Gotham; then
+if [ -e "~/Library/Preferences/com.googlecode.iterm2.plist" ] && cat ~/Library/Preferences/com.googlecode.iterm2.plist | grep Gotham; then
   echo -e "👍\033[0;96m gotham theme\033[0m installed"
 else
   echo -e "✅ download \033[0;96mgotham theme\033[0m"
@@ -33,8 +33,11 @@ if [ -e /Applications/Sublime\ Text.app ]; then
 else
   echo -e "✅\033[0;96m install Sublime Text\033[0m"
   brew install --cask sublime-text > /dev/null 2>&1
+  # here we manually create these dirs because these dirs don't get created until we run subl for the first time
+  mkdir -p "~/Library/Application Support/Sublime Text/Installed Packages"
+  mkdir -p "~/Library/Application Support/Sublime Text/Packages/User"
   echo -e "✅\033[0;96m download Sublime Package Control\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Installed Packages/Package Control.sublime-package" -L "https://packagecontrol.io/Package%20Control.sublime-package"
+  curl -sSo "~/Library/Application Support/Sublime Text/Installed Packages/Package Control.sublime-package" -L "https://packagecontrol.io/Package%20Control.sublime-package"
 fi
 
 # install command-line fuzzy finder
@@ -58,15 +61,15 @@ echo -e "✅ download \033[0;96m .zshrc\033[0m"
 curl -sSo ~/.zshrc -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/.zshrc"
 echo -e "✅ source \033[0;96m .zshrc\033[0m"
 
-if [ -e "/Users/jackfan/Library/Application Support/Sublime Text" ]; then
+if [ -e "~/Library/Application Support/Sublime Text" ]; then
   echo -e "✅ download \033[0;96m sublime preference settings\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Preferences.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Preferences.sublime-settings"
+  curl -sSo "~/Library/Application Support/Sublime Text/Packages/User/Preferences.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Preferences.sublime-settings"
   echo -e "✅ download \033[0;96m sublime keymap\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-keymap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-keymap"
+  curl -sSo "~/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-keymap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-keymap"
   echo -e "✅ download \033[0;96m sublime mousemap\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-mousemap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-mousemap"
+  curl -sSo "~/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-mousemap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-mousemap"
   echo -e "✅ download \033[0;96m sublime package list\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Package Control.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Package%20Control.sublime-settings"
+  curl -sSo "~/Library/Application Support/Sublime Text/Packages/User/Package Control.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Package%20Control.sublime-settings"
 else
   echo -e "🔸sublime text may not be installed; skipping settings/bindings download"
 fi
