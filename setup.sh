e39ef2cf300f3dae0d9f39524303c043447fca6c
Jack Fan <jackfan108@gmail.com>
2024-06-11 05:09:35 -0400

    sublime install fixes

--- setup.sh
+++ setup.sh
@@ -28,13 +28,13 @@ else
 fi
 
 # install sublime text
-if [ -e /Applications/Sublime Text.app ]; then
+if [ -e /Applications/Sublime\ Text.app ]; then
   echo -e "👍\033[0;96m Sublime Text\033[0m is installed"
 else
   echo -e "✅\033[0;96m install Sublime Text\033[0m"
   brew install --cask sublime-text > /dev/null 2>&1
   echo -e "✅\033[0;96m download Sublime Package Control\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package" -L "https://packagecontrol.io/Package%20Control.sublime-package"
+  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Installed Packages/Package Control.sublime-package" -L "https://packagecontrol.io/Package%20Control.sublime-package"
 fi
 
 # install command-line fuzzy finder
@@ -58,15 +58,15 @@ echo -e "✅ download \033[0;96m .zshrc\033[0m"
 curl -sSo ~/.zshrc -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/.zshrc"
 echo -e "✅ source \033[0;96m .zshrc\033[0m"
 
-if [ -e "/Users/jackfan/Library/Application Support/Sublime Text 3/Packages/User" ]; then
+if [ -e "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User" ]; then
   echo -e "✅ download \033[0;96m sublime preference settings\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Preferences.sublime-settings"
+  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Preferences.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Preferences.sublime-settings"
   echo -e "✅ download \033[0;96m sublime keymap\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-keymap"
+  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-keymap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-keymap"
   echo -e "✅ download \033[0;96m sublime mousemap\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-mousemap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-mousemap"
+  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-mousemap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default%20(OSX).sublime-mousemap"
   echo -e "✅ download \033[0;96m sublime package list\033[0m"
-  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Package%20Control.sublime-settings"
+  curl -sSo "/Users/jackfan/Library/Application Support/Sublime Text/Packages/User/Package Control.sublime-settings" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Package%20Control.sublime-settings"
 else
-  echo -e "🔸sublime text may not be installed; skipping keymap/mousemap download"
+  echo -e "🔸sublime text may not be installed; skipping settings/bindings download"
 fi
