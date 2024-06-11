394b9eb1a617590c3e4f990fa00a113150fe2dd0
Jack Fan <jackfan108@gmail.com>
2024-06-11 02:57:40 -0400

    download sublime keymap

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # install homebrew
 if command -v brew >/dev/null 2>&1; then
-  echo -e "\033[0;96m Homebrew\033[0m is installed. Version=$(brew --version)"
+  echo -e "👍\033[0;96m Homebrew\033[0m is installed. Version=$(brew --version)"
 else
-  echo -e "\033[0;96m Installing Homebrew\033[0m"
+  echo -e "✅\033[0;96m Installing Homebrew\033[0m"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jackfan/.zprofile
@@ -11,20 +11,25 @@ fi
 
 # install command-line fuzzy finder
 if command -v fzf >/dev/null 2>&1; then
-  echo -e "\033[0;96m fzf\033[0m is installed; version=$(fzf --version)"
-  fzf --version
+  echo -e "👍\033[0;96m fzf\033[0m is installed; version=$(fzf --version)"
 else
-  echo -e "\033[0;96m Installing fzf (command-line fuzzy finder)\033[0m"
+  echo -e "✅\033[0;96m Installing fzf (command-line fuzzy finder)\033[0m"
   brew install fzf
 fi
 
 # Difftastic is a structural diff tool that compares files based on their syntax.
 if command -v fzf >/dev/null 2>&1; then
-  echo -e "\033[0;96m difftastic\033[0m is installed"
+  echo -e "👍\033[0;96m difftastic\033[0m is installed"
 else
   brew install difftastic
 fi
 
 # create and download zshrc
+echo -e "✅ download \033[0;96m .zshrc\033[0m"
 curl -o ~/.zshrc -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/.zshrc"
+echo -e "✅ source \033[0;96m .zshrc\033[0m"
+
+
+echo -e "✅ download \033[0;96m .zshrc\033[0m"
+curl -o "/Users/jackfan/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap" -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/Default (OSX).sublime-keymap"
 source ~/.zshrc
