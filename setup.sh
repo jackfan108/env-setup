d1f2d0a572b4eb98f5cee22967999772d82548fe
Jack Fan <jackfan108@gmail.com>
2024-06-11 02:42:05 -0400

    update install script to check

--- setup.sh
+++ setup.sh
@@ -1,18 +1,29 @@
 # install homebrew
 if command -v brew >/dev/null 2>&1; then
-  echo "Homebrew is installed."
-  brew --version
+  echo -e "\033[0;96m Homebrew\033[0m is installed. Version=$(brew --version)"
 else
-  echo "Installing Homebrew"
+  echo -e "\033[0;96m Installing Homebrew\033[0m"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jackfan/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # install command-line fuzzy finder
-brew install fzf
+if command -v fzf >/dev/null 2>&1; then
+  echo -e "\033[0;96m fzf\033[0m is installed; version=$(fzf --version)"
+  fzf --version
+else
+  echo -e "\033[0;96m Installing fzf (command-line fuzzy finder)\033[0m"
+  brew install fzf
+fi
 
 # Difftastic is a structural diff tool that compares files based on their syntax.
-brew install difftastic
+if command -v fzf >/dev/null 2>&1; then
+  echo -e "\033[0;96m difftastic\033[0m is installed"
+else
+  brew install difftastic
+fi
 
 # create and download zshrc
 curl -o ~/.zshrc -L "https://raw.githubusercontent.com/jackfan108/env-setup/main/.zshrc"
